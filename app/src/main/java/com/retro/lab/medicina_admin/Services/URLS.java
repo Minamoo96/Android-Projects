a864a76cdeba2f79d95cf1edb3a94d3d6a4aff32
HassanPC <midoo3000@gmail.com>
2022-06-08 06:15:22 -0700

    admin side app adding urls for backend side

---
+++ app/src/main/java/com/retro/lab/medicina_admin/Services/URLS.java
@@ -0,0 +1,11 @@
+package com.retro.lab.medicina_admin.Services;
+
+public class URLS {
+
+    private static final String ROOT_URL = "http://jalald.000webhostapp.com/api/api.php?apicall=";
+    public static final String URL_REGISTER = ROOT_URL+"signup";
+    public static final String URL_GET_All_Medicines = ROOT_URL+"index";
+    public static final String URL_GET_All_PHARMACIES = ROOT_URL+"index";
+    public static final String URL_GET_All_USERS = ROOT_URL+"index";
+    public static final String URL_SEARCH = ROOT_URL+"search";
+}
